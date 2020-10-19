b90b270cca6b0193dd5c04ca705b71c9c9280e6b
Quentin Brogly <qbrogly@gmail.com>
2020-10-19 10:10:04 -0700

    Add url shortener endpoint

--- server.js
+++ server.js
@@ -1,37 +1,52 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require("express");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("dns");
 
-var cors = require('cors');
+const app = express();
 
-var app = express();
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+// todo: mongoose.connect(process.env.DB_URI);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+// middleware to parse post request body
+app.use(bodyParser.json());
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+// url shortener endpoint
+app.post("/api/shorturl/new", function (req, res, next) {
+  const url = req.body.url;
+  dns.lookup(url.hostname, (err, addr) => {
+    if (err && err.code === "ENOTFOUND") {
+      res.json({ error: "invalid URL" });
+    } else {
+      res.json({
+        original_url: url,
+        short_url: url.length,
+      });
+    }
+  });
+});
+
+app.get("/api/shorturl/");
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
